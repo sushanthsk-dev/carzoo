7e9dc240285eb7fc5af3ab15f60be4381acf6658
unknown <prajithgowda23@gmail.com>
2021-08-23 22:47:58 +0700

    Added no order screen

--- src/features/profile/screens/my-order.screen.js
+++ src/features/profile/screens/my-order.screen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { TouchableOpacity, ScrollView, View } from "react-native";
+import { TouchableOpacity, ScrollView, View,Image } from "react-native";
 import styled from "styled-components/native";
 import axios from "axios";
 import { Header } from "../../../components/header/header.component";
@@ -15,9 +15,17 @@ const OrderContainer = styled(ScrollView)`
 `;
 
 const Container = styled(View)`
-  margin-top: 70px;
+  margin-top: 30px;
   align-items: center;
   justify-content: center;
+  height:100%;
+ 
+`;
+const ImageContainer = styled(Image)`
+
+margin-left:20px;
+margin-bottom:20px;
+
 `;
 
 export const MyOrderScreen = ({ navigation }) => {
@@ -58,7 +66,9 @@ export const MyOrderScreen = ({ navigation }) => {
         </OrderContainer>
       ) : (
         <Container>
-          <Text variant="title">No Order</Text>
+          <ImageContainer source={require("../../../../assets/no-order.png")}/>
+          <Text variant="title">No Service Order</Text>
+
         </Container>
       )}
     </SafeArea>
